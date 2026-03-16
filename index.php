e35de69001a3404dac0dcb3fb41e24b8cd239a1b
dev <dev@tip.ovh>
2026-03-16 20:48:28 +0100

    Cambio lang=es

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $app_name = getenv('APP_NAME') ?: 'NONE';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
